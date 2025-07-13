ba91886c52338cfa809f2f6611d8f144984fe6ab
Harshit <kartiklohani2004@gmail.com>
2025-07-13 07:08:07 +0530

    docker image created on modal

--- train.py
+++ train.py
@@ -8,16 +8,18 @@ from model import DenseNet121
 from torch.utils.data import Dataset
 import os
 
-app = modal.Stub("find-your-pill-train")
+volume = modal.Volume.from_name("pill-classifier-data", create_if_missing=True)
+model_volume = modal.Volume.from_name("pill-classifier-model", create_if_missing=True)
+
+app = modal.App("find-your-pill-train",
+                 secrets=[modal.Secret.from_name("huggingface-token")],
+                 volumes={"/data": volume, "/model": model_volume})
 
 image = (modal.Image.debian_slim()
         .pip_install_from_requirements("requirements.txt")
         .apt_install(["wget", "ffmpeg", "unzip"])
         .add_local_python_source("model")
         )
-
-volume = modal.Volume.from_name("pill-classifier-data", create_if_missing=True)
-model_volume = modal.Volume.from_name("pill-classifier-model", create_if_missing=True)
 
 class PillDataset(Dataset):
     def __init__(self, csv_file: str, image_dir: str, transform=None):
@@ -37,11 +39,29 @@ class PillDataset(Dataset):
             image = self.transform(image)
         label = self.data.iloc[idx, 1]
         return image, label
+    
+@app.function(image=image, secrets=[modal.Secret.from_name("huggingface-token")], volumes={"/data": volume})
+def download_data():
+    print("Downloading data...")
+    try:
+        import os
+        from huggingface_hub import login
+        from datasets import load_dataset
+
+        login(token=os.environ["HUGGINGFACE_SECRET_KEY"])
+        # Creating dir for image data
+        os.makedirs("/data/sd-198-metadata", exist_ok=True)
+        # loading the dataset
+        dataset = load_dataset("resyhgerwshshgdfghsdfgh/SD-198")
+        dataset.save_to_disk("/data/sd-198-metadata")
+    except Exception as e:
+        print(f"Error in downloading data: {e}")
+
 
 @app.function(image=image, volumes={"/data": volume, "/model": model_volume}, gpu="A10G", timeout=3600*5)
-def train(x):
+def train():
     print("This code is running on a remote worker!")
-    return x**2
+    return 3**2
 
 
 @app.local_entrypoint()
